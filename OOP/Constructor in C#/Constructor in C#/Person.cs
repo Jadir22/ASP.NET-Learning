aeea0625a97800d0dbc99ed11f3ad658a82fb9b8
Junayed Islam <junayedislamjadir5721@gmail.com>
2026-07-10 20:49:25 +0600

    Constructor in C#

---
+++ OOP/Constructor in C#/Constructor in C#/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor_in_C_
+{
+    class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            Firstname = firstName;
+            Lastname = lastName;
+        }
+
+        public string GetFullName()
+        {
+            return Firstname+" "+Lastname;
+        }
+    }
+}
---
+++ OOP/Constructor in C#/Constructor in C#/Program.cs
@@ -0,0 +1,5 @@
+﻿using Constructor_in_C_;
+
+Person p = new Person("Junayed", "Islam");
+string fullName = p.GetFullName();
+Console.WriteLine(fullName);
